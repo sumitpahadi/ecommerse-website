8e1153ed9b64730090e0aa7058770eb24e3a3a97
sumitpahadi <sumitrawat1102000@gmail.com>
2023-10-16 13:24:19 +0530

    fix issue api

--- frontend/src/Component/pages/Success.js
+++ frontend/src/Component/pages/Success.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../App.css";
 import suceess from "../assests/payment-successful-5343612-4475257.png"
 function Success() {
+  localStorage.removeItem("cartData")
   return (
     <div className='success-container'>
         <img src={suceess} alt=""  className="success-image"/>
